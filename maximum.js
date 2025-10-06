23aff96e259f7e18d3fc68091d85691a854d693f
Rahat Hossain <rahat1470.com@gmail.com>
2025-10-07 01:41:32 +0600

    update

---
+++ maximum.js
@@ -0,0 +1,10 @@
+const num1 = 5;
+const num2 = 6;
+
+if (num1 > num2) {
+    console.log("first number is maximum number");
+} else if (num2 > num1) {
+    console.log("secound number is maximum number");
+} else {
+    console.log("two number is equal");
+}
